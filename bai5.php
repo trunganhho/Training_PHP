ea1e0af5f4ce78746ad0a573f11ea805ed3a9346
TrungAnh <hohoangtrunganh17122000@gmail.com>
2021-12-09 11:02:48 +0700

    Finish

--- bai5.php
+++ bai5.php
@@ -4,8 +4,8 @@
         <h1>Dãy số</h1>
         <?php 
         for($i = 1; $i <= 100; $i++){
-            echo '<p ' . ($i % 2 == 0 ? 'style="font-weight:bold;"' : '') . 
-            '>'. $i . '</p>';
+            echo '<span ' . ($i % 2 == 0 ? 'style="font-weight:bold;"' : '') . 
+            '>'. $i . ' </span>';
         }
         ?>
     </body>
